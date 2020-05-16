bd505761aae02be7224bd4faa1c29bd043f0248f
Yash Bhalodi <yashbhalodi007@gmail.com>
2020-05-16 18:13:56 +0530

    First attempt to understand BLoC

---
+++ lib/box_bloc.dart
@@ -0,0 +1,75 @@
+import 'dart:core';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
+
+class BoxBloc {
+  double initialX = 0;
+  double initialY = 0;
+  int score = 0;
+  bool gameON = false;
+  BehaviorSubject<Widget> _subjectBox;
+  BehaviorSubject<bool> _subjectGame;
+  BehaviorSubject<int> _subjectScore;
+
+  BoxBloc({this.initialX, this.initialY}) {
+    _subjectBox = BehaviorSubject.seeded(_drawBox());
+    _subjectGame = BehaviorSubject.seeded(false);
+    _subjectScore = BehaviorSubject.seeded(0);
+  }
+
+  Stream<Widget> get boxObservable => _subjectBox.stream;
+
+  Stream<bool> get gameObservable => _subjectGame.stream;
+
+  Stream<int> get scoreObservable => _subjectScore.stream;
+
+  void gameToggle() async {
+    if (this.gameON) {
+      gameON = false;
+      _subjectGame.sink.add(false);
+    } else {
+      gameON = true;
+      score = 0;
+      _subjectScore.sink.add(score);
+      _subjectGame.sink.add(true);
+      while (gameON) {
+        initialY = (Random().nextDouble() * 2) - 1;
+        initialX = (Random().nextDouble() * 2) - 1;
+        await Future.delayed(
+          Duration(milliseconds: (Random().nextInt(10) + 5) * 100),
+          () => _subjectBox.sink.add(_drawBox()),
+        );
+      }
+    }
+  }
+
+  Widget _drawBox() {
+    return Align(
+      alignment: Alignment(initialX, initialY),
+      child: GestureDetector(
+        onTap: () {
+          if (gameON) {
+            score++;
+            _subjectScore.sink.add(score);
+          }
+        },
+        child: Container(
+          height: (Random().nextDouble() + 0.1) * 100,
+          decoration: ShapeDecoration(
+            shape: CircleBorder(),
+            color: Color.fromARGB(
+                255, Random().nextInt(256), Random().nextInt(256), Random().nextInt(256)),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void dispose() {
+    _subjectBox.close();
+    _subjectGame.close();
+    _subjectScore.close();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:counterbloc/box_bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BLoC',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        accentColor: Colors.tealAccent,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  BoxBloc _boxBloc = BoxBloc(initialX: 0.0, initialY: 0.0);
+
+  @override
+  void dispose() {
+    _boxBloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BLoC"),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right:16.0),
+            child: StreamBuilder(
+              stream: _boxBloc.scoreObservable,
+              builder: (_, snapshot) {
+                return Text(
+                  snapshot.data.toString(),
+                  style: Theme.of(context).textTheme.display2,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: StreamBuilder(
+          stream: _boxBloc.boxObservable,
+          builder: (_, snapshot) {
+            if (snapshot.hasData) {
+              return snapshot.data;
+            } else {
+              return Container();
+            }
+          },
+        ),
+      ),
+      floatingActionButton: StreamBuilder<Object>(
+        stream: _boxBloc.gameObservable,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return FloatingActionButton(
+              onPressed: _boxBloc.gameToggle,
+              child: snapshot.data ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+            );
+          } else {
+            return null;
+          }
+        },
+      ),
+    );
+  }
+}
